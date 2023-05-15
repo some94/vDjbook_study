00c82c1d79b528fbe5bf904712c2d3f4e09a096a
Donghwi Kim <ehdgnl0904@naver.com>
2023-05-15 17:34:41 +0900

    FEAT: 메인 페이지 path 추가

--- vDjbook/settings.py
+++ vDjbook/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'vDjbook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'bookmark/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- vDjbook/urls.py
+++ vDjbook/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from vDjbook.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # class-based views
+    # vDjbook
+    path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
 ]
