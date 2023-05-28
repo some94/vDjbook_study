dafac76879abc44d6fda74405f072236d873a5f1
Donghwi Kim <ehdgnl0904@naver.com>
2023-05-28 19:44:15 +0900

    FEAT: owner 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(verbose_name='TITLE', max_length=50)
@@ -10,6 +12,7 @@ class Post(models.Model):
     create_dt = models.DateTimeField('CREATE DATE', auto_now_add=True)
     modify_dt = models.DateTimeField('MODIFY DATE', auto_now=True)
     tags = TaggableManager(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
 
     class Meta:
         verbose_name = 'post'
@@ -28,3 +31,8 @@ class Post(models.Model):
 
     def get_next(self):
         return self.get_next_by_modify_dt()
+
+    # save 메서드를 재정의. 모델 객체의 내용을 DB에 저장하는 메서드
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)   # 부모 클래스의 save 메서드를 호출해 객체 내용을 테이블에 반영하는 원래 기능을 수행하도록 함
