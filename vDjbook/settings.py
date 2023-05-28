d1f299308422b583e64c54db60bcfb258e601056
Donghwi Kim <ehdgnl0904@naver.com>
2023-05-28 19:01:04 +0900

    FEAT: widget_tweaks 앱 사용 설정 및 LOGIN_REDIRECT_URL 추가

--- vDjbook/settings.py
+++ vDjbook/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'taggit.apps.TaggitAppConfig',
     'taggit_templatetags2',
     'photo.apps.PhotoConfig',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 TAGGIT_CASE_INSENSITIVE = True
 TAGGIT_LIMIT = 50
